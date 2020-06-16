6009aece3133f26712f0eeb558abbd0a83ce1962
Tim Vu <timvu@timvuc0tc9pg3qd.lan>
2020-06-16 10:12:45 -0400

    added prettier & used on all files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,40 +6,41 @@
       </IonToolbar>
     </IonHeader>
     <IonLabel>
-      <RouterLink to= "/">Home Page</RouterLink>
+      <RouterLink to="/">Home Page</RouterLink>
     </IonLabel>
   </div>
 </template>
 
 <script lang="ts">
-  import {
-    IonHeader, IonTitle, IonToolbar, IonLabel
-  } from '@modus/ionic-vue'
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String,
-    },
-    components: {
-      IonHeader, IonTitle, IonToolbar, IonLabel
-    },
-  };
+import { IonHeader, IonTitle, IonToolbar, IonLabel } from '@modus/ionic-vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  components: {
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonLabel,
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
