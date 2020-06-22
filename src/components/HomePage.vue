9e96a00e2b786eb2f49c17317ad9798abd341deb
Tim Vu <timvu@timvuc0tc9pg3qd.lan>
2020-06-22 08:38:09 -0400

    cards now render properly on homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="page">
-    <IonContent>
-      <div class="list">
-        <ion-card v-for="category in categories" :key="category.id">
-          <RouterLink :to="category.path">
-            <ion-img class="hero" :src="category.image" :alt="category.name" />
-          </RouterLink>
-          <ion-card-header>
-            <ion-card-subtitle>Featured</ion-card-subtitle>
-            <ion-card-title>{{ category.name }}</ion-card-title>
-            <ion-fab vertical="center" horizontal="end">
-              <ion-fab-button :href="category.path">
-                View
-              </ion-fab-button>
-            </ion-fab>
-          </ion-card-header>
-        </ion-card>
-      </div>
-    </IonContent>
-  </div>
+  <IonContent>
+    <div class="list">
+      <ion-card v-for="category in categories" :key="category.id">
+        <RouterLink :to="category.path">
+          <ion-img class="hero" :src="category.image" :alt="category.name" />
+        </RouterLink>
+        <ion-card-header>
+          <ion-card-subtitle>Featured</ion-card-subtitle>
+          <ion-card-title>{{ category.name }}</ion-card-title>
+          <ion-fab vertical="center" horizontal="end">
+            <ion-fab-button :href="category.path">
+              View
+            </ion-fab-button>
+          </ion-fab>
+        </ion-card-header>
+      </ion-card>
+    </div>
+  </IonContent>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,45 @@
 <template>
-  <div class="tabs">
-    <IonTabs>
-      <IonTab tab="home">
-        <HomePage :categories="categories" />
-      </IonTab>
-      <IonTab tab="tshirts">
-        <CollectionList heading="tshirts" />
-      </IonTab>
-      <IonTab tab="hoodies">
-        <CollectionList heading="hoodies" />
-      </IonTab>
-      <IonTab tab="shorts">
-        <CollectionList heading="shorts" />
-      </IonTab>
-      <IonTab tab="jeans">
-        <CollectionList heading="jeans" />
-      </IonTab>
-      <IonTab tab="cart">
-        <IonLabel>Cart page</IonLabel>
-      </IonTab>
-      <template v-slot:top>
-        <IonTabBar>
-          <IonTitle>Vueport Shop</IonTitle>
-          <IonTabButton tab="home">
-            <IonLabel>Home</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tshirts">
-            <IonLabel>T-Shirts</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="shorts">
-            <IonLabel>Shorts</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="jeans">
-            <IonLabel>Jeans</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="cart">
-            <IonIcon icon="cart" />
-            <IonLabel>Cart</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </template>
-    </IonTabs>
-  </div>
+  <IonTabs>
+    <IonTab tab="home">
+      <HomePage :categories="categories" />
+    </IonTab>
+    <IonTab tab="tshirts">
+      <CollectionList heading="tshirts" />
+    </IonTab>
+    <IonTab tab="hoodies">
+      <CollectionList heading="hoodies" />
+    </IonTab>
+    <IonTab tab="shorts">
+      <CollectionList heading="shorts" />
+    </IonTab>
+    <IonTab tab="jeans">
+      <CollectionList heading="jeans" />
+    </IonTab>
+    <IonTab tab="cart">
+      <IonLabel>Cart page</IonLabel>
+    </IonTab>
+    <template v-slot:top>
+      <IonTabBar>
+        <IonTitle>Vueport Shop</IonTitle>
+        <IonTabButton tab="home">
+          <IonLabel>Home</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tshirts">
+          <IonLabel>T-Shirts</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="shorts">
+          <IonLabel>Shorts</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="jeans">
+          <IonLabel>Jeans</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="cart">
+          <IonIcon icon="cart" />
+          <IonLabel>Cart</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </template>
+  </IonTabs>
 </template>
 
 <script lang="ts">
