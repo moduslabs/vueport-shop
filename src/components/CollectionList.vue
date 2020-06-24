f781db4ea90e4cd10ef2a57748726238640eb822
TCModus <63469617+TCModus@users.noreply.github.com>
2020-06-24 09:10:32 -0400

    Feature/product list (#5)
    * added mock product data from Grgur's deno script, imported that data as a prop into the CollectionList conponent to render the list of products for a category
    
    * added ion card containg the image and text content for the products
    
    * shrunk pictures with styling
    
    * implemented ion grid but not getting multple columns, category image no longer static
    
    Co-authored-by: Tom <tomcollins@MacBook-Pro.local>
    Co-authored-by: Grgur Grisogono <grgur@grgur.com>

--- src/components/CollectionList.vue
+++ src/components/CollectionList.vue
@@ -1,7 +1,24 @@
 <template>
   <NavBar />
   <IonContent>
-    <h1>{{ heading }}</h1>
+    <ion-img :alt="category.title" :src="category.image" />
+    <ion-label>{{ heading }}</ion-label>
+    <ion-label>{{ data.length + ' items' }}</ion-label>
+    <ion-grid fixed="true">
+      <ion-row>
+        <ion-col col-2>
+          <ion-card v-for="product in data" :key="product.id">
+            <img
+              class="image"
+              :alt="product.description"
+              :src="product.images[1]"
+            />
+            <p>{{ product.title }}</p>
+            <p>{{ product.price }}</p>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
   </IonContent>
 </template>
 
@@ -13,11 +30,22 @@ import NavBar from '../components/NavBar.vue'
 export default defineComponent({
   name: 'CollectionList',
   components: {
-    NavBar,
     IonContent,
+    NavBar,
   },
   props: {
     heading: String,
+    data: Array,
+    category: Object,
   },
 })
 </script>
+<style scoped>
+.list {
+  padding-bottom: 3rem;
+}
+.image {
+  height: 20%;
+  width: 50%;
+}
+</style>
