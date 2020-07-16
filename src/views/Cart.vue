ca8b03248bd8771d06238fb18b84a5595309745f
Tim Vu <timmyvu2020@gmail.com>
2020-07-16 13:32:45 -0400

    trying to fix cart nested routes - checkout & ordercompleted

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,91 +1,31 @@
 <template>
   <IonTab tab="cart">
-    <IonRouterView />
-    <IonContent v-if="$route.path === '/cart'">
-      <IonText v-if="cart.items.length === 0">
-        Your <IonIcon icon="cart" class="ion-hide-sm-down" /> is empty
-      </IonText>
-      <IonList v-if="cart.items.length > 0">
-        <IonText> You have {{ cart.totalItems }} items in your cart </IonText>
-        <IonItem v-for="item in cart.items" :key="item.id">
-          <IonGrid>
-            <IonRow>
-              <IonCol>
-                <IonImg
-                  :alt="item['products'].title"
-                  :src="item['products'].images[0]"
-                />
-              </IonCol>
-              <IonCol>
-                <IonLabel class="title">{{ item['products'].title }}</IonLabel>
-              </IonCol>
-              <IonCol>
-                <IonLabel>{{ currency(item['products'].price) }}</IonLabel>
-              </IonCol>
-              <IonCol>
-                <IonButton @click="removeItem(item)">X</IonButton>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonItem>
-        <IonLabel>{{ 'Total: ' + currency(cart.totalCost) }}</IonLabel>
-        <RouterLink to="/cart/checkout">
-          <IonButton fill="outline" color="dark">
-            Checkout
-          </IonButton>
-        </RouterLink>
-      </IonList>
-    </IonContent>
+    <IonRouterView v-slot="{ Component, transitionProps }">
+      <Suspense>
+        <template #default>
+          <Transition v-bind="transitionProps">
+            <component :is="Component" />
+          </Transition>
+        </template>
+      </Suspense>
+    </IonRouterView>
   </IonTab>
 </template>
 
-<script type="ts">
-import {
-  IonContent,
-  IonText,
-  IonTab,
-  IonIcon,
-  IonList,
-  IonItem,
-  IonButton,
-  IonLabel,
-  IonImg,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonRouterView,
-} from '@modus/ionic-vue'
-import cart from '../composables/cart/index'
+<script lang="ts">
+import { IonTab, IonRouterView } from '@modus/ionic-vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'Cart',
+  name: 'Home',
   components: {
-    IonContent,
-    IonText,
     IonTab,
-    IonIcon,
-    IonList,
-    IonItem,
-    IonButton,
-    IonLabel,
-    IonImg,
-    IonGrid,
-    IonRow,
-    IonCol,
     IonRouterView,
-  },
-  setup() {
-    const currency = cart.getCurrencyFormat()
-    function removeItem(item){
-      cart.remove(item)
-    }
-    return { cart, currency, removeItem }
   },
 })
 </script>
 <style scoped>
-ion-label {
-  word-wrap: break-word;
+.list {
+  padding-bottom: 3rem;
 }
 </style>
