92a5da8bb40d259c9961a11b91c2565e87d310dc
Michael Tintiuc <contact@michaeltintiuc.com>
2020-07-16 17:01:13 +0300

    Fix category/product navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,32 @@
 <template>
   <IonTab tab="collections">
-    <IonRouterView />
-    <IonContent v-if="$route.path === '/'">
-      <div class="list">
-        <IonCard v-for="category in this.categories" :key="category.id">
-          <RouterLink :to="`/category/${category.id}`">
-            <IonImg class="hero" :src="category.image" :alt="category.title" />
-          </RouterLink>
-          <IonCardHeader>
-            <IonCardSubtitle>Featured</IonCardSubtitle>
-            <IonCardTitle>{{ category.title }}</IonCardTitle>
-            <IonFab vertical="center" horizontal="end">
-              <RouterLink
-                :to="`/category/${category.id}`"
-                aria-label="View this category"
-              >
-                <IonFabButton>
-                  View
-                </IonFabButton>
-              </RouterLink>
-            </IonFab>
-          </IonCardHeader>
-        </IonCard>
-      </div>
+    <IonContent>
+      <IonRouterView v-slot="{ Component, transitionProps }">
+        <Suspense>
+          <template #default>
+            <Transition v-bind="transitionProps">
+              <component :is="Component" />
+            </Transition>
+          </template>
+        </Suspense>
+      </IonRouterView>
+      <CategoryList v-if="$route.path === '/'" />
     </IonContent>
   </IonTab>
 </template>
 
 <script lang="ts">
-import {
-  IonContent,
-  IonTab,
-  IonCard,
-  IonImg,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonFab,
-  IonFabButton,
-  IonRouterView,
-} from '@modus/ionic-vue'
+import { IonContent, IonTab, IonRouterView } from '@modus/ionic-vue'
 import { defineComponent } from 'vue'
-import { RouterLink } from 'vue-router'
-import useCategories from '@/composables/categories'
+import CategoryList from '@/components/CategoryList.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    RouterLink,
     IonContent,
     IonTab,
-    IonCard,
-    IonImg,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
-    IonFab,
-    IonFabButton,
     IonRouterView,
-  },
-  async setup() {
-    const { categories } = await useCategories()
-    return { categories }
+    CategoryList,
   },
 })
 </script>
