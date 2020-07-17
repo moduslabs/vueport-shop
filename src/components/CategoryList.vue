506f2853d5816092b77905edd9dfbe048573d0d9
Tim Vu <timmyvu2020@gmail.com>
2020-07-17 11:21:54 -0400

    styling edits, slide options

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,7 +1,11 @@
 <template>
   <IonContent>
-    <IonSlides :options="slideOpts" pager="true">
-      <IonSlide v-for="category in this.categories" :key="category.id">
+    <IonSlides :options="slideOpts" pager="true" class="container">
+      <IonSlide
+        v-for="category in this.categories"
+        :key="category.id"
+        class="slide"
+      >
         <IonCard>
           <RouterLink :to="`/category/${category.id}`">
             <IonImg class="hero" :src="category.image" :alt="category.title" />
@@ -62,8 +66,10 @@ export default defineComponent({
     const { categories } = await useCategories()
     const slideOpts = {
       initialSlide: 1,
+      slidesPerView: 'auto',
       speed: 400,
       spaceBetween: 30,
+      loop: true,
     }
     return { categories, slideOpts }
   },
@@ -72,5 +78,29 @@ export default defineComponent({
 <style scoped>
 .list {
   padding-bottom: 3rem;
+}
+.container {
+  width: 100%;
+  height: 100%;
+}
+.slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  width: 80%;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
 }
 </style>
