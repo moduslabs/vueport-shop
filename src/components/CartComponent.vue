7e007b272e067fcb39ab71784b4c42817ccb650a
Tim Vu <timmyvu2020@gmail.com>
2020-07-21 11:01:57 -0400

    cards in cart

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -1,34 +1,58 @@
 <template>
-  <IonContent>
+  <IonContent class="cart">
     <IonText v-if="cart.totalItems === 0">
       Your <IonIcon icon="cart" /> is empty
     </IonText>
 
     <IonList v-else>
+      <IonTitle>Your Cart</IonTitle>
       <IonText> You have {{ cart.totalItems }} items in your cart </IonText>
       <IonItem v-for="[skuId, item] in Array.from(cart.items)" :key="skuId">
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonImg :alt="item.product.title" :src="item.product.images[0]" />
-            </IonCol>
-            <IonCol>
-              <IonLabel class="title"
-                >{{ item.product.title }}, {{ item.variant.title }} ({{
-                  item.quantity
-                }})</IonLabel
-              >
-            </IonCol>
-            <IonCol>
-              <IonLabel>{{ currency(item.price) }}</IonLabel>
-            </IonCol>
-            <IonCol>
-              <IonButton @click="removeItem(skuId)">X</IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+        <IonCard class="card">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonThumbnail class="product-image-container">
+                  <IonImg
+                    class="product-image"
+                    :alt="item.product.title"
+                    :src="item.product.images[0]"
+                  />
+                </IonThumbnail>
+              </IonCol>
+              <IonCol>
+                <b>
+                  <IonText color="primary">{{ item.product.title }} </IonText>
+                </b>
+                <br />
+                <i>
+                  {{ item.variant.title }}
+                </i>
+              </IonCol>
+              <IonCol>
+                Qty: <IonLabel>{{ item.quantity }}</IonLabel>
+              </IonCol>
+              <IonCol>
+                <IonText>
+                  {{ currency(item.price) }}
+                </IonText>
+              </IonCol>
+              <IonCol>
+                <IonButton
+                  @click="removeItem(skuId)"
+                  class="remove"
+                  fill="clear"
+                  color="dark"
+                >
+                  X
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
       </IonItem>
       <IonLabel>{{ 'Total: ' + currency(cart.totalCost) }}</IonLabel>
+      <br />
       <RouterLink to="/cart/checkout">
         <IonButton fill="outline" color="dark">
           Checkout
@@ -51,6 +75,9 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonThumbnail,
+  IonCard,
+  IonTitle,
 } from '@modus/ionic-vue'
 import cart from '../composables/cart'
 import { defineComponent } from 'vue'
@@ -69,6 +96,9 @@ export default defineComponent({
     IonGrid,
     IonRow,
     IonCol,
+    IonThumbnail,
+    IonCard,
+    IonTitle,
   },
   setup() {
     const currency = cart.getCurrencyFormat()
@@ -80,7 +110,21 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-ion-label {
-  word-wrap: break-word;
+.card {
+  width: 100%;
+}
+.cart {
+  text-align: center;
+}
+.product-image-container {
+  width: 4rem;
+  height: 4rem;
+}
+
+.product-image {
+  border-radius: 0.2rem;
+}
+.remove {
+  text-align: center;
 }
 </style>
