946dd2e0c963fe39a83094d1051403d528b6e64e
Grgur Grisogono <grgur@grgur.com>
2020-07-28 11:56:38 +0200

    Category card (#30)
    * Category card
    
    * a11y

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,100 +1,39 @@
 <template>
   <IonContent>
-    <IonSlides
-      :options="slideOpts"
-      pager="true"
-      class="container ion-hide-md-down"
-    >
+    <IonSlides :options="slideOpts" pager="true" class="container">
       <IonSlide
         v-for="category in this.categories"
         :key="category.id"
         class="slide"
       >
-        <IonCard class="card-desktop">
-          <RouterLink :to="`/category/${category.id}`">
-            <RouterLink :to="`/category/${category.id}`">
-              <IonImg
-                :src="category.image"
-                :alt="category.title"
-                class="desktop"
-              />
-            </RouterLink>
-            <div class="card-content">
-              <IonCardHeader>
-                <IonCardSubtitle>Featured</IonCardSubtitle>
-                <IonCardTitle>{{ category.title }}</IonCardTitle>
-              </IonCardHeader>
-              <IonCardContent>
-                {{ category.description }}
-              </IonCardContent>
-              <RouterLink :to="`/category/${category.id}`">
-                <IonFab vertical="end" horizontal="end">
-                  <IonFabButton>
-                    <IonIcon icon="pricetags" />
-                  </IonFabButton>
-                </IonFab>
-              </RouterLink>
-            </div>
-          </RouterLink>
-        </IonCard>
-      </IonSlide>
-    </IonSlides>
-    <IonSlides
-      :options="mobileSlideOpts"
-      pager="true"
-      class="container ion-hide-md-up"
-    >
-      <IonSlide
-        v-for="category in this.categories"
-        :key="category.id"
-        class="slide"
-      >
-        <IonCard class="card-mobile">
-          <RouterLink :to="`/category/${category.id}`">
-            <IonImg
+        <RouterLink
+          :to="`/category/${category.id}`"
+          class="category-link"
+          :ariaLabel="`See products in the ${category.title} Category`"
+        >
+          <article>
+            <img
               :src="category.image"
               :alt="category.title"
-              class="mobile"
+              class="category-image"
+              decoding="async"
+              loading="lazy"
+              aria-hidden="true"
             />
-            <div>
-              <IonCardHeader>
-                <IonCardSubtitle>Featured</IonCardSubtitle>
-                <IonCardTitle>{{ category.title }}</IonCardTitle>
-              </IonCardHeader>
-              <IonCardContent>
-                {{ category.description }}
-              </IonCardContent>
-              <RouterLink :to="`/category/${category.id}`">
-                <IonFab vertical="end" horizontal="end">
-                  <IonFabButton>
-                    <IonIcon icon="pricetags" />
-                  </IonFabButton>
-                </IonFab>
-              </RouterLink>
+
+            <div class="category-title">
+              <h2>{{ category.title }}</h2>
             </div>
-          </RouterLink>
-        </IonCard>
+          </article>
+        </RouterLink>
       </IonSlide>
     </IonSlides>
   </IonContent>
 </template>
 
 <script lang="ts">
-import {
-  IonCard,
-  IonImg,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonCardContent,
-  IonFab,
-  IonFabButton,
-  IonContent,
-  IonSlides,
-  IonSlide,
-  IonIcon,
-} from '@modus/ionic-vue'
-import { defineComponent } from 'vue'
+import { IonContent, IonSlides, IonSlide } from '@modus/ionic-vue'
+import { defineComponent, ref } from 'vue'
 import { RouterLink } from 'vue-router'
 import useCategories from '@/composables/categories'
 
@@ -102,75 +41,106 @@ export default defineComponent({
   name: 'CategoryList',
   components: {
     RouterLink,
-    IonCard,
-    IonImg,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
-    IonCardContent,
-    IonFab,
-    IonFabButton,
     IonContent,
     IonSlides,
     IonSlide,
-    IonIcon,
   },
   async setup() {
     const { categories } = await useCategories()
-    const slideOpts = {
-      initialSlide: 0,
-      // speed: 400,
-      spaceBetween: 100,
-      centeredSlides: true,
-      keyboard: true,
+    const isDesktop = ref(false)
+    const slideOpts = ref({})
+
+    const mql = window.matchMedia('(min-width: 800px)')
+
+    function onWidthChange(query: MediaQueryList) {
+      isDesktop.value = query.matches
+
+      if (query.matches) {
+        // desktop options
+        slideOpts.value = {
+          slidesPerView: 3,
+          spaceBetween: 10,
+          loop: true,
+          grabCursor: true,
+          autoplay: {
+            delay: 5500,
+            disableOnInteraction: true,
+          },
+        }
+      } else {
+        slideOpts.value = {
+          // mobile options
+          slidesPerView: 'auto',
+          spaceBetween: 10,
+          loop: true,
+          autoplay: {
+            delay: 3500,
+            disableOnInteraction: true,
+          },
+        }
+      }
     }
-    const mobileSlideOpts = {
-      initialSlide: 0,
-    }
-    const here = () => {
-      console.log('here')
-    }
-    return { categories, slideOpts, mobileSlideOpts, here }
+
+    mql.addListener(onWidthChange)
+    onWidthChange(mql)
+
+    return { categories, slideOpts }
   },
 })
 </script>
-<style scoped>
-.mobile {
-  width: 100vw;
-  height: calc(60vh / (16 / 9));
-}
-.desktop {
-  height: 65%;
-}
-.container {
-  height: 100%;
-}
-.slide {
-  text-align: center;
-  font-size: 18px;
-  background: #fff;
-  height: 100%;
 
-  /* Center slide text vertically */
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
+<style scoped>
+.container {
+  padding: 0 1rem;
 }
-.card-desktop {
-  height: 80vh;
-  width: 80vw;
+
+.slide {
+  font-size: 1.25rem;
+  height: auto;
+  width: 85vw;
 }
-.card-mobile {
-  height: 83vh;
+
+.card-content {
+  text-align: left;
+}
+
+article {
+  position: relative;
+  height: 30rem;
+  border-radius: 0.825rem;
+  overflow: hidden;
+  box-shadow: 16px 4px 0 rgba(0, 0, 0, 0.5);
+  box-shadow: 1px 2px 16px 2px rgba(0, 0, 0, 0.5);
+  margin: 2rem 0;
+}
+
+.category-title h2 {
+  position: absolute;
+  top: 2rem;
+  left: 1.25rem;
+  color: white;
+  font-size: 3rem;
+}
+
+.category-title {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.category-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.category-link {
   width: 100%;
 }
 </style>
