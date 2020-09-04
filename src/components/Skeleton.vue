6faa5b76fc462ad96488a87729487374d1bb9a82
Tom Collins <thomas.collins@moduscreate.com>
2020-09-04 10:23:08 -0400

    reformatted skeleton componenent with prettier

--- src/components/Skeleton.vue
+++ src/components/Skeleton.vue
@@ -3,7 +3,7 @@
     <IonList>
       <IonListHeader>
         <IonLabel>
-          <IonSkeletonText animated style="width: 20%"></IonSkeletonText>
+          <IonSkeletonText animated style="width: 20%;"></IonSkeletonText>
         </IonLabel>
       </IonListHeader>
       <IonItem v-for="(n, idx) in Array(5)" :key="idx">
@@ -12,13 +12,13 @@
         </IonAvatar>
         <IonLabel>
           <h3>
-            <IonSkeletonText animated style="width: 50%"></IonSkeletonText>
+            <IonSkeletonText animated style="width: 50%;"></IonSkeletonText>
           </h3>
           <p>
-            <IonSkeletonText animated style="width: 80%"></IonSkeletonText>
+            <IonSkeletonText animated style="width: 80%;"></IonSkeletonText>
           </p>
           <p>
-            <IonSkeletonText animated style="width: 60%"></IonSkeletonText>
+            <IonSkeletonText animated style="width: 60%;"></IonSkeletonText>
           </p>
         </IonLabel>
         <IonThumbnail slot="end">
